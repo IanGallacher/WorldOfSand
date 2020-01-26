d86c4430cd90b51e629ec9139108f1c2f1c77069
Ian Gallacher <igallacher@outlook.com>
2020-01-25 20:21:15 -0800

    add GameStateManager

---
+++ Assets/_scripts/GameStateManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+enum GameState {
+	Paused,
+	Play,
+}
+
+enum GameStateTransition {
+	Pause,
+	Reset,
+	Run,
+}
+
+enum ControlMode {
+	Edit,
+	Erase,
+	Manipulate,
+}
+
+
+[System.Serializable]
+public class GameStateTransationEvent : UnityEvent<GameStateTransition>
+{
+
+}
+
+public class GameStateManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameState _currentGameState;
+
+    // Awake is called once for the lifetime of the script, before start.
+    // Used for setting up variables.
+    void Awake()
+    {
+        _currentGameState = GameState.Paused;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
